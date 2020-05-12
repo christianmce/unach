73f15f8f6d1f44d4333f89ea0007b2cc4ce9bdcc
ChristianMCE <cmce@unach.mx>
2020-05-11 22:58:04 -0500

    Create lectura_archivobinario.c

---
+++ programas_en_C/lectura_archivobinario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct {
+	char nombre[20];
+	char apellido[20];
+	char telefono[15];
+} registro;
+
+int main(int argc, char const *argv[])
+{
+	FILE *fichero;
+
+	if ((fichero = fopen( "nombres.dat", "rb" )) == NULL) {
+		printf( "No se puede abrir el fichero.\n" );
+		exit( 1 );
+	}
+
+	while (!feof(fichero)) {
+  
+		if (fread( &registro, sizeof(registro), 1, fichero )) {
+			printf( "Nombre: %s\n",   registro.nombre );
+			printf( "Apellido: %s\n", registro.apellido);
+			printf( "Teléfono: %s\n", registro.telefono);
+      
+		}
+	}
+
+	fclose( fichero );
+}
