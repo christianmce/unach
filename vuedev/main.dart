723b5b6671c81fde312389a6273aa223c4c55c67
ChristianMCE <cmce@unach.mx>
2022-11-03 13:22:52 -0600

    Create main.dart

---
+++ vuedev/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo UNACH'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _LoginData {
+  String email = '';
+  String password = '';
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Login'),
+      ),
+      body: new Container(
+          padding: new EdgeInsets.all(20.0),
+          child: new Form(
+            key: this._formKey,
+            child: new ListView(
+              children: <Widget>[
+                new TextFormField(
+                    keyboardType: TextInputType
+                        .emailAddress, // Use email input type for emails.
+                    decoration: new InputDecoration(
+                        hintText: 'you@example.com',
+                        labelText: 'E-mail Address')),
+                new TextFormField(
+                    obscureText: true, // Use secure text for passwords.
+                    decoration: new InputDecoration(
+                        hintText: 'Password',
+                        labelText: 'Enter your password')),
+                new Container(
+                  width: screenSize.width,
+                  child: new RaisedButton(
+                    child: new Text(
+                      'Login',
+                      style: new TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () => null,
+                    color: Colors.blue,
+                  ),
+                  margin: new EdgeInsets.only(top: 20.0),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
