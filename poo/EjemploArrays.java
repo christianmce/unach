0be67f3d58f9b72b509d4df899ef413fe8de4477
ChristianMCE <cmce@unach.mx>
2024-04-08 15:34:26 -0600

    Create EjemploArrays.java

---
+++ poo/EjemploArrays.java
@@ -0,0 +1,13 @@
+     
+//ArrayList de Objetos <Object> 
+    ArrayList<String> lista = new ArrayList<>();
+    lista.add("MEXICO");
+    lista.add("CANADA");
+    lista.add("ITALIA");
+    
+    for (int i=0;i<lista.size();i++) {      
+      System.out.println(lista.get(i));
+    }
+
+
+              
