0cf39db61b8324de51c66c8686eb94cd152e7b15
ChristianMCE <cmce@unach.mx>
2025-03-24 20:26:48 -0600

    Update 3_SplitsDuplicates.py

--- BigData/3_SplitsDuplicates.py
+++ BigData/3_SplitsDuplicates.py
@@ -21,3 +21,19 @@ data = {'Address': ['123 Main St, Anytown, CA, 12345', '456 1st Ave, Anycity, NY
 df = pd.DataFrame(data)
 df[['Street', 'City', 'State', 'Zip']] = df['Address'].str.split(', ', expand=True)
 print(df)
+
+# *************************************************************************************************
+
+# *****  df.insert(loc, column, value)  ********
+
+df = pd.DataFrame({'B': [1, 2, 3], 'C': [4, 5, 6]})
+Out: 
+   B  C
+0  1  4
+1  2  5
+2  3  6
+
+idx = 0
+new_col = [7, 8, 9]  # can be a list, a Series, an array or a scalar   
+df.insert(loc=idx, column='A', value=new_col)
+
